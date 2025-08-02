ab2864612139fa15c31b863165db136d66d37370
DeepakAC3 <deepakpradip29@gmail.com>
2025-08-02 12:02:12 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path"; // built in
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -11,13 +12,18 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve(); // this is the current directory where the server.js file is located
 
 // connectDB(); // if we leave it like this, then it will connect only after the server starts. instead we will wrap app.listen in a specific way
 
 //middleware
-app.use(cors({
-    origin:"http://localhost:5173",
-  }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
 app.use(express.json()); // read from the req.body before doing anything else
 app.use(rateLimiter); // apply rate limiting middleware
 // parse the json body: req.body
@@ -31,9 +37,17 @@ app.use(rateLimiter); // apply rate limiting middleware
 
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))); // serve static files from the frontend build directory
+  // this is the directory where the frontend build files are located
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    // this will send the index.html file for any route that is not handled by the API
+  });
+}
+
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("Server started on PORT: ", PORT); // now the DB is connected before the server starts
   });
 });
-
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,8 +2,12 @@ import axios from "axios";
 // this is an axios instance that can be used throughout the application
 // to make API calls to the backend server
 // it has a base URL set to the backend server's API endpoint
+
+// in production, no localhost so we have to make this dynamic
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: BASE_URL,
 });
 
 export default api;
