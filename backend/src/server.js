8c8c83712e5ec5c3c10c7175acd4ae7c25d6a24f
DeepakAC3 <deepakpradip29@gmail.com>
2025-09-01 15:10:49 +0530

    getting ready to add authentication

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,6 +21,9 @@ if (process.env.NODE_ENV !== "production") {
   app.use(
     cors({
       origin: "http://localhost:5173",
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
     }),
   );
 }
