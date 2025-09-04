d3cdf1d8d30f30274c41e60a9e0efc22c702c460
DeepakAC3 <deepakpradip29@gmail.com>
2025-09-04 21:30:49 +0530

    fixed the no ui issue and include clerk as a dependency

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["@clerk/clerk-react"],
-    },
+  optimizeDeps: {
+    include: ["@clerk/clerk-react"],
+  },
+  ssr: {
+    noExternal: ["@clerk/clerk-react"],
   },
 });
